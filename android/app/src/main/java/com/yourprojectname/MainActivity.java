66658fd018a9e1910f6453d4c81048cbac1c6a43
Reza Lukman <lukman.reza@akasia.id>
2023-06-07 18:07:59 +0700

    rename project

--- android/app/src/main/java/com/yourprojectname/MainActivity.java
+++ android/app/src/main/java/com/yourprojectname/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "YourProjectName";
+    return "rn_surplus";
   }
 
   /**
