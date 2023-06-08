17bfa518c886242dbb53f1f2d5c8ce259c725d3e
Reza Lukman <lukman.reza@akasia.id>
2023-06-08 19:43:03 +0700

    update dependancies

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 /**
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
