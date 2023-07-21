dd4e9d2eef32ffc3cddafe12db4f71c0e8ec4f17
Reza Lukman <lukman.reza@akasia.id>
2023-07-21 12:54:06 +0700

    re-configure

--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -14,7 +14,7 @@ class MainApplicationTurboModuleManagerDelegate
  public:
   // Adapt it to the package you used for your Java class.
   static constexpr auto kJavaDescriptor =
-      "Lcom/rn_surplus/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+      "Lcom/holidayo/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/rn_surplus/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/holidayo/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
