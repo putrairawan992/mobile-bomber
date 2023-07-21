5b51a6947d3c247ff9562ab90ab32850aa0bda90
Reza Lukman <lukman.reza@akasia.id>
2023-07-21 13:35:01 +0700

    update dependancies

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import React, { type PropsWithChildren } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import EStyleSheet from 'react-native-extended-stylesheet';
 import { Provider, shallowEqual, useSelector } from 'react-redux';
 import AppModalToastContext from './src/context/AppModalToastContext';
 import Routes from './src/navigation/Routes';
 import store from './src/store';
 import ThemeProvider from './src/theme/ThemeProvider';
+
+EStyleSheet.build();
 
 function App() {
   return (
