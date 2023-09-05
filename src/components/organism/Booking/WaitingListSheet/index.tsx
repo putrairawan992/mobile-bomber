c53c07d8d5e030a0807d97a5fd1254eab9cef633
Reza Lukman <ahmadiah.reza@rekava.build>
2023-09-05 18:49:56 +0700

    add waiting list sheet and table UI

---
+++ src/components/organism/Booking/WaitingListSheet/index.tsx
@@ -0,0 +1,184 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useCallback} from 'react';
+import {
+  TouchableOpacity,
+  Text as RNText,
+  LayoutAnimation,
+  View,
+} from 'react-native';
+import useTheme from '../../../../theme/useTheme';
+import {Button, Gap, Section, Text, TextInput} from '../../../atoms';
+import {ArrowLeft} from 'iconsax-react-native';
+import {Colors} from '../../../../theme';
+import {WAITING_LIST_TEXT} from '../../../../utils/data';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+
+interface WaitingListSheetProps {
+  hasBackNavigation: boolean;
+  onBackNavigation: () => void;
+  step: number;
+  onChangeStep: (step: number) => void;
+  onFinish: () => void;
+}
+
+export const WaitingListSheet = ({
+  hasBackNavigation,
+  onBackNavigation,
+  step,
+  onChangeStep,
+  onFinish,
+}: WaitingListSheetProps) => {
+  const theme = useTheme();
+  let EMAIL_REGX = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .required('Email address is required')
+        .matches(EMAIL_REGX, 'Invalid email address'),
+    }),
+    validateOnChange: false,
+    enableReinitialize: true,
+    onSubmit: () => onNextStep(2),
+  });
+  const onNextStep = useCallback((value: number) => {
+    LayoutAnimation.configureNext({
+      duration: 500,
+      update: {
+        type: 'easeInEaseOut',
+      },
+    });
+    onChangeStep(value);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Section
+      padding="0px 16px"
+      style={{flex: 1}}
+      backgroundColor={theme?.colors.SECTION}>
+      {hasBackNavigation && step === 1 && (
+        <TouchableOpacity
+          style={{position: 'absolute', zIndex: 999, left: 16, top: 16}}
+          onPress={() => {
+            onBackNavigation();
+            formik.setFieldError('email', undefined);
+          }}>
+          <ArrowLeft size={24} color={theme?.colors.ICON} />
+        </TouchableOpacity>
+      )}
+      <Section isCenter>
+        <Gap height={15} />
+        <Text
+          variant="base"
+          fontWeight="bold"
+          label={
+            step === 1 ? 'Waiting List' : 'You are registered on waiting list'
+          }
+          color={theme?.colors.WARNING}
+          textAlign="center"
+        />
+      </Section>
+      <Gap height={30} />
+      {step === 1 ? (
+        <Section>
+          <Text
+            variant="base"
+            fontWeight="poppins-regular"
+            label="WHAT IS WAITING LIST"
+            textAlign="center"
+            color={Colors['black-10']}
+          />
+          <Gap height={20} />
+          <Text
+            label={WAITING_LIST_TEXT[0]}
+            textAlign="center"
+            color={Colors['black-10']}
+          />
+          <Gap height={16} />
+          <RNText
+            style={{
+              fontFamily: 'Inter',
+              fontWeight: '500',
+              fontSize: 14,
+              color: Colors['black-10'],
+              textAlign: 'center',
+            }}>
+            {WAITING_LIST_TEXT[1]}
+            <RNText
+              style={{
+                fontFamily: 'Inter-Italic',
+                fontWeight: '500',
+                fontSize: 14,
+                color: Colors['black-10'],
+                textAlign: 'center',
+                fontStyle: 'italic',
+              }}>
+              {WAITING_LIST_TEXT[2]}
+            </RNText>
+          </RNText>
+          <Gap height={16} />
+          <Text
+            label={WAITING_LIST_TEXT[3]}
+            textAlign="center"
+            color={Colors['black-10']}
+          />
+          <Section padding="20px 0px">
+            <TextInput
+              value={formik.values.email}
+              errorText={formik.errors.email}
+              onChangeText={value => {
+                formik.setFieldValue('email', value);
+                formik.setFieldError('email', undefined);
+              }}
+              placeholder="Input your email address"
+            />
+          </Section>
+        </Section>
+      ) : (
+        <Text
+          label="When the host cancel their booking then you we will
+          inform you and you can back to bomber and book
+          your table"
+          textAlign="center"
+          color={Colors['black-10']}
+        />
+      )}
+
+      <View
+        style={{
+          position: 'absolute',
+          width: '100%',
+          bottom: 32,
+          alignSelf: 'center',
+        }}>
+        {step === 1 ? (
+          <>
+            <Button
+              type="primary"
+              onPress={() => formik.handleSubmit()}
+              title="Continue Whitelist"
+            />
+            <Button
+              type="textButton"
+              onPress={() => {
+                onBackNavigation();
+                formik.setFieldError('email', undefined);
+              }}
+              title="Cancel Join"
+            />
+          </>
+        ) : (
+          <Button
+            type="primary"
+            onPress={onFinish}
+            title="Gotcha, back to home"
+          />
+        )}
+      </View>
+    </Section>
+  );
+};
--- src/interfaces/BookingInterface.ts
+++ src/interfaces/BookingInterface.ts
@@ -5,6 +5,7 @@ export interface TableInterface {
   text: string;
   minOrder: number;
   price: number;
+  isAvailable?: boolean;
 }
 
 export interface ListItemInterface {
